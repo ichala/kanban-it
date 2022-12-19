8800eedf134c491443ec7896cb541711a68aec8f
Ali Jendoubi <alijandhd@gmail.com>
2022-12-19 14:31:33 +0100

    Add Layout

---
+++ src/config/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../Context/theme';
+import Navbar from './Navbar/Navbar';
+
+const Layout = ({ children }) => {
+  const { Theme } = useContext(ThemeContext);
+  return (
+    <main data-theme={Theme} className="bg-base-300 h-screen">
+      <Navbar />
+      <div className="my-3 p-3">
+        <div className="flex flex-grow  mt-4 container-webkit overflow-auto">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
