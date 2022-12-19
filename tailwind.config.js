bad561374fe45e47662b8bb52a91ba3540f6d29d
Ali Jendoubi <alijandhd@gmail.com>
2022-12-19 12:34:17 +0100

    Setup Daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
+    fontFamily: {
+      display: ['Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji'],
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
