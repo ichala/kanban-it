49c8e530fe4c3238242b4c62558a5cfd9882d288
Ali Jendoubi <alijandhd@gmail.com>
2022-12-20 02:49:24 +0100

    Delete Column Functions

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { BsCircleFill, BsPlusLg } from 'react-icons/bs';
+import React, { useContext } from 'react';
+import { BsCircleFill, BsFillTrashFill } from 'react-icons/bs';
+import { StorageContext } from '../../config/Context/storage';
+import NewCard from '../Card/NewCard';
 
-const Column = ({ children }) => (
-  <div className="flex flex-col flex-shrink-0 w-72 ">
-    <div className="flex items-center justify-center flex-shrink-0  p-2">
-      <span className=" text-sm font-semibold flex gap-2 items-center">
-        <BsCircleFill className="text-primary" />
-        Column Name
-      </span>
-      <button
-        type="button"
-        className="flex items-center justify-center w-6 h-6 ml-auto text-primary rounded"
-      >
-        <BsPlusLg />
-      </button>
+const Column = ({ children, color, id }) => {
+  const { setStorageData } = useContext(StorageContext);
+
+  const deleteColumn = (id) => {
+    setStorageData((prev) => prev.filter((e) => e.id !== id));
+  };
+  return (
+    <div className="flex flex-col flex-shrink-0 w-72 ">
+      <div className="flex group items-center justify-center flex-shrink-0  p-2">
+        <span className=" text-sm font-semibold flex gap-2 items-center">
+          <BsCircleFill color={color} />
+          Column Name
+        </span>
+        <div
+          className="group-hover:visible flex  items-center invisible justify-center w-6 h-6 ml-auto text-primary rounded"
+        >
+          <BsFillTrashFill className="text-error" onClick={() => deleteColumn(id)} />
+        </div>
+      </div>
+      <div className="flex flex-col rounded h-[700px] bg-base-100 my-2 p-3 overflow-auto column-webkit">
+        {children}
+        <NewCard />
+      </div>
     </div>
-    <div className="flex flex-col rounded h-[700px] bg-base-100 my-2 p-3 overflow-auto column-webkit">
-      {children}
-    </div>
-  </div>
-);
+  );
+};
 
 export default Column;
