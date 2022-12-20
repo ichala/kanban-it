d37da15f60dc514469006aee8151c913d58bf49c
Ali Jendoubi <alijandhd@gmail.com>
2022-12-20 02:48:33 +0100

    Add new Card button

---
+++ src/components/Card/NewCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewCard = () => (
+  <div
+    className="relative flex flex-col text-primary/30 hover:text-primary/100  items-center outline md:outline-dashed p-4 mt-3 rounded-lg cursor-pointer"
+  >
+    <h2 className="text-md font-bold text-center ">New Card</h2>
+  </div>
+);
+
+export default NewCard;
