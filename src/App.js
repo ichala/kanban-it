d4c978f0b564bece6fa6b6dbc7bf76f1a0a5eadc
Ali Jendoubi <alijandhd@gmail.com>
2022-12-20 02:48:24 +0100

    Add Toaster

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { useRoutes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from './config/Context/theme';
+import { StorageProvider } from './config/Context/storage';
 import routesConfig from './config/routes';
 
 function App() {
   const routes = useRoutes(routesConfig);
   return (
     <>
-      <ThemeProvider>
-        { routes }
-      </ThemeProvider>
+      <StorageProvider>
+        <ThemeProvider>
+          <Toaster />
+          { routes }
+        </ThemeProvider>
+      </StorageProvider>
     </>
   );
 }
