7d83030dbbcbe16692ea1bbd5e1c9e39bff9b902
Ali Jendoubi <alijandhd@gmail.com>
2022-12-20 03:55:32 +0100

    Update Style

--- src/config/Layout/Layout.jsx
+++ src/config/Layout/Layout.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar/Navbar';
 const Layout = ({ children }) => {
   const { Theme } = useContext(ThemeContext);
   return (
-    <main data-theme={Theme} className="bg-base-300 h-screen">
+    <main data-theme={Theme} className="bg-base-300 min-h-screen">
       <Navbar />
       <div className="my-3 p-3">
         <div className="flex flex-grow  mt-4 container-webkit overflow-auto">
