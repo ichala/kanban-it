cc1f9c61fb8627e2145fd52f69072dc050b80593
Ali Jendoubi <alijandhd@gmail.com>
2022-12-21 21:55:29 +0100

    Update Column

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -3,7 +3,7 @@ import { BsCircleFill, BsFillTrashFill } from 'react-icons/bs';
 import { StorageContext } from '../../config/Context/storage';
 import NewCard from '../Card/NewCard';
 
-const Column = ({ children, color, id }) => {
+const Column = ({ children, data }) => {
   const { setStorageData } = useContext(StorageContext);
 
   const deleteColumn = (id) => {
@@ -13,18 +13,18 @@ const Column = ({ children, color, id }) => {
     <div className="flex flex-col flex-shrink-0 w-72 ">
       <div className="flex group items-center justify-center flex-shrink-0  p-2">
         <span className=" text-sm font-semibold flex gap-2 items-center">
-          <BsCircleFill color={color} />
-          Column Name
+          <BsCircleFill color={data.color} />
+          {data.title.charAt(0).toUpperCase() + data.title.slice(1)}
         </span>
         <div
           className="group-hover:visible flex  items-center invisible justify-center w-6 h-6 ml-auto text-primary rounded"
         >
-          <BsFillTrashFill className="text-error" onClick={() => deleteColumn(id)} />
+          <BsFillTrashFill className="text-error" onClick={() => deleteColumn(data.id)} />
         </div>
       </div>
       <div className="flex flex-col rounded h-[700px] bg-base-100 my-2 p-3 overflow-auto column-webkit">
         {children}
-        <NewCard id={id} />
+        <NewCard id={data.id} />
       </div>
     </div>
   );
