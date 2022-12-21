83d05cb901d7f9e9e0b21df1dae8ac4401d2e7c5
Ali Jendoubi <alijandhd@gmail.com>
2022-12-21 21:54:54 +0100

    Update Card Layout

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BiCommentDetail } from 'react-icons/bi';
-import { BsThreeDotsVertical } from 'react-icons/bs';
+import { FaTasks } from 'react-icons/fa';
 import { MdDateRange } from 'react-icons/md';
 import moment from 'moment';
 
@@ -9,13 +8,7 @@ const Card = ({ data }) => (
     className="relative flex flex-col items-start p-4 mt-3 bg-primary rounded-lg cursor-pointer bg-opacity-90 group hover:bg-opacity-100"
     draggable="true"
   >
-    <button
-      type="button"
-      className="absolute top-0 right-0 flex items-center justify-center hidden w-5 h-5 mt-3 mr-2 text-gray-500 rounded hover:bg-gray-200 hover:text-gray-700 group-hover:flex"
-    >
-      <BsThreeDotsVertical className="w-4 h-4 fill-current" />
 
-    </button>
     <span className="flex items-center h-6 px-3 text-xs font-semibold text-primary bg-base-100 rounded-full">
       {data.title.charAt(0).toUpperCase() + data.title.slice(1)}
     </span>
@@ -30,8 +23,8 @@ const Card = ({ data }) => (
         </span>
       </div>
       <div className="flex items-center">
-        <BiCommentDetail className="w-4 h-4" />
-        <span className="ml-1 leading-none">4</span>
+        <FaTasks className="w-4 h-4" />
+        <span className="ml-1 leading-none">{data.tasks?.length || 0}</span>
       </div>
 
       <img
