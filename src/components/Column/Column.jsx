6b3d27719c6c606c439bddfd3dac1c5789894d66
Ali Jendoubi <alijandhd@gmail.com>
2022-12-24 02:00:50 +0100

    Update Column Style

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BsCircleFill, BsFillTrashFill } from 'react-icons/bs';
+import { BsArrowLeftRight, BsCircleFill, BsFillTrashFill } from 'react-icons/bs';
 import { StorageContext } from '../../config/Context/storage';
 import NewCard from './NewCard';
 
@@ -11,21 +11,35 @@ const Column = ({ children, data }) => {
   };
   return (
     <div className="flex flex-col flex-shrink-0 w-72 ">
-      <div className="flex group items-center justify-center flex-shrink-0  p-2">
-        <span className=" text-sm font-semibold flex gap-2 items-center">
+      <div className="flex group items-center justify-between  flex-shrink-0  p-2">
+        <span className=" text-sm font-semibold flex gap-2 items-center ">
           <BsCircleFill color={data.color} />
           {data.title.charAt(0).toUpperCase() + data.title.slice(1)}
         </span>
-        <div
-          className="group-hover:visible flex  items-center invisible justify-center w-6 h-6 ml-auto text-primary rounded"
-        >
-          <BsFillTrashFill className="text-error" onClick={() => deleteColumn(data.id)} />
+        <div className="flex ">
+          <div
+            className="group-hover:visible flex  items-center invisible  justify-center w-6 h-6 ml-auto text-primary rounded"
+          >
+            <label htmlFor="edit-columns">
+              <BsArrowLeftRight className="cursor-pointer" />
+            </label>
+
+          </div>
+          <div
+            className="group-hover:visible flex  items-center invisible  justify-center w-6 h-6 ml-auto text-primary rounded"
+          >
+
+            <BsFillTrashFill className="text-error cursor-pointer" onClick={() => deleteColumn(data.id)} />
+          </div>
+
         </div>
+
       </div>
-      <div className="flex flex-col rounded h-[700px] bg-base-100 my-2 p-3 overflow-x-hidden overflow-y-auto column-webkit">
+      <div className="flex shadow-md flex-col rounded h-[700px] bg-base-100 my-2 p-3 overflow-x-hidden overflow-y-auto column-webkit">
         {children}
         <NewCard id={data.id} />
       </div>
+
     </div>
   );
 };
