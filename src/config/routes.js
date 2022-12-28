fd8b7e911deced5daaa38898a2f29367326c22b4
ichala <alijandhd@gmail.com>
2022-12-28 13:32:04 +0100

    Add protected routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,31 +3,41 @@ import Home from '../Pages/Home/Home';
 import New from '../Pages/Home/Cards/NewCard';
 import NewColumn from '../Pages/Home/Columns/NewColumn';
 import EditCard from '../Pages/Home/Cards/EditCard';
+import Protected from './Context/protected_routes';
+import Login from '../Pages/Login/Login';
 
 const routesConfig = [
   {
-    path: '/',
-    element: <Home />,
+    element: <Protected />,
     children: [
       {
-        path: '/:id/new',
-        element: <New />,
-      },
-      {
-        path: '/:id/view',
-        element: <CardDetails />,
-      },
-      {
-        path: '/:id/edit',
-        element: <EditCard />,
-      },
-      {
-        path: '/new',
-        element: <NewColumn />,
+        path: '/',
+        element: <Home />,
+        children: [
+          {
+            path: '/:id/new',
+            element: <New />,
+          },
+          {
+            path: '/:id/view',
+            element: <CardDetails />,
+          },
+          {
+            path: '/:id/edit',
+            element: <EditCard />,
+          },
+          {
+            path: '/new',
+            element: <NewColumn />,
+          },
+        ],
       },
     ],
   },
-
+  {
+    path: '/login',
+    element: <Login />,
+  },
 ];
 
 export default routesConfig;
