e79593b9bc4878cf09e762cb72acd1419eaf7a23
ichala <alijandhd@gmail.com>
2022-12-28 14:30:21 +0100

    Add User image to navbar & logout btn

--- src/config/Layout/Navbar/Navbar.jsx
+++ src/config/Layout/Navbar/Navbar.jsx
@@ -1,55 +1,59 @@
 import { signOut } from 'firebase/auth';
-import React from 'react';
+import React, { useContext } from 'react';
 import { BsFillKanbanFill } from 'react-icons/bs';
+import { AuthContext } from '../../Context/auth';
 import { auth } from '../../firebase';
 import ThemeSelect from './ThemeSelect';
 
-const Navbar = () => (
-  <div className="navbar bg-base-00">
-    <div className="flex-1">
-      <a href="#s" className="btn btn-ghost normal-case text-xl gap-1">
-        <BsFillKanbanFill className="text-primary" />
-        KanBan-it!
-      </a>
-    </div>
-    <div className="flex-none">
-      <div className="dropdown dropdown-end">
-        <ThemeSelect />
+const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <div className="navbar bg-base-00">
+      <div className="flex-1">
+        <button type="button" className="btn btn-ghost normal-case text-xl gap-1">
+          <BsFillKanbanFill className="text-primary" />
+          KanBan-it!
+        </button>
       </div>
-      <div className="dropdown dropdown-end">
-        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
-            <img src="https://placeimg.com/80/80/people" alt="tets" />
-          </div>
-        </label>
-        <ul
-          tabIndex={0}
-          className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <a href="#s" className="justify-between">
-              Profile
-              <span className="badge">New</span>
-            </a>
-          </li>
-          <li>
-            <a href="#s">Settings</a>
-          </li>
-          <li>
-            <button
-              type="button"
-              className="text-red-500"
-              onClick={() => {
-                signOut(auth);
-              }}
-            >
-              Logout
-            </button>
-          </li>
-        </ul>
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <ThemeSelect />
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src={currentUser.image} alt="tets" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a href="#s" className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a href="#s">Settings</a>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="text-red-500"
+                onClick={() => {
+                  signOut(auth);
+                }}
+              >
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
