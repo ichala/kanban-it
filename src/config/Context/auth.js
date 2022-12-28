9f7fa388d7faa5e293662ebb2905f80ea0786228
ichala <alijandhd@gmail.com>
2022-12-28 13:05:05 +0100

    Create auth context

---
+++ src/config/Context/auth.js
@@ -0,0 +1,37 @@
+import { createContext, useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [Loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const image = user.photoURL || '/assets/images/no-image.png';
+        setCurrentUser({ name: user.displayName, image, id: user.uid });
+        setLoading(false);
+      } else {
+        setCurrentUser(null);
+        setLoading(false);
+      }
+    });
+    return () => {
+      unsub();
+    };
+  }, []);
+  if (!Loading) {
+    return (
+      <AuthContext.Provider value={{ currentUser }}>
+        {children}
+      </AuthContext.Provider>
+    );
+  }
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      Loading
+    </AuthContext.Provider>
+  );
+};
